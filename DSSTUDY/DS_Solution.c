37e6a72510c67a71f045334f8c575c846689ceda
CHUNIEE\21400 <21400337@handong.edu>
2017-03-29 00:17:38 +0900

    Homework #1 Problem2

--- DSSTUDY/DS_Solution.c
+++ DSSTUDY/DS_Solution.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
+#include <malloc.h>
 
 int main(void)
 {
 	int size = 10;
-	int ary[10];
 	int *ptr = NULL;
 	int i = 0;
 
-	for (i = 0; i < 100; i = (i+10)) {
-		ary[i/10] = i;
-	}	
-
-	ptr = ary;
-
-	printf("ary = %p, ary + 3 = %p, &ary[3] = %p \n", ary, ary + 3, &ary[3]);
-	printf("ptr = %p, ptr + 3 = %p, &ptr[3] = %p \n", ptr, ptr + 3, &ptr[3]);
-
-	printf("ary = { ");
-
-	for (i = 0; i < 100; i = (i + 10)) {
-		printf("%d, ", ary[i / 10]);
+	ptr = (int *)malloc(sizeof(int) * size);
+	for (i = 0; i < 100; i = i + 10) {
+		*(ptr + i / 10) = i;
 	}
 
-	printf("}\n");
+	printf("ptr = %p, ptr + 3 = %p, &ptr[3] = %p \n", ptr, ptr + 3, &ptr[3]);
+
+	for (i = 0; i < size; i++) {
+		printf(" %d ", ptr[i]);
+	}
+
+	printf("\n");
+
+	free(ptr);
 
 	return 0;
 }
